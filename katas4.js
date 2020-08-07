1db1442c41250dec93bb3b729e99657fc46da922
Davis Peixoto <davis@kenzie.com.br>
2020-08-07 19:06:19 -0300

    forkable version

---
+++ katas4.js
@@ -0,0 +1,13 @@
+const gotCitiesCSV = "King's Landing,Braavos,Volantis,Old Valyria,Free Cities,Qarth,Meereen";
+
+const lotrCitiesArray = ["Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"];
+
+const bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit";
+
+function kata1() {
+    // implemente aqui o código
+}
+
+// kata1();
+
+// 
